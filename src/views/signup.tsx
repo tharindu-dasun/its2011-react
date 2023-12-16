0b15c0be62b254afe9d300722fd9a5690916f6fa
tharindu-dasun <tharindu.dasunccp@gmail.com>
2023-12-16 21:15:00 +0530

    update my-article

--- src/views/signup.tsx
+++ src/views/signup.tsx
@@ -1,73 +1,70 @@
-import React from 'react';
 import { Link } from "react-router-dom";
 import Input from "./../component/input/input";
 
-class Signup extends React.Component<any, any> {
-    render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | Iterable<React.ReactNode> | React.ReactPortal | boolean | any | null | undefined {
-        return(
-            <section className={'flex justify-center items-center p-5'}>
-                <div className={'w-fit p-10 border shadow-xl rounded-xl'}>
+function Signup(): JSX.Element {
+    return(
+        <section className={'flex justify-center items-center p-5'}>
+            <div className={'w-fit p-10 border shadow-xl rounded-xl'}>
 
-                    <img src="https://logodix.com/logo/1597040.png" title="logo" alt="logo" className={'w-24 m-auto'}/>
+                <img src="https://logodix.com/logo/1597040.png" title="logo" alt="logo" className={'w-24 m-auto'}/>
 
-                    <div className={'text-2xl font-bold text-green-600 text-center mt-5'}>
-                        Sign Up
-                    </div>
+                <div className={'text-2xl font-bold text-green-600 text-center mt-5'}>
+                    Sign Up
+                </div>
 
-                    <div className={'mt-5 min-w-[400px]'}>
+                <div className={'mt-5 min-w-[400px]'}>
 
-                        <div className={'flex'}>
-
-                            <Input
-                                type={'text'}
-                                name={'fname'}
-                                label={'First Name'}
-                                placeholder={'Enter your first name'}
-                                optional={false}/>
-
-                            <Input
-                                type={'text'}
-                                name={'lname'}
-                                label={'Last Name'}
-                                placeholder={'Enter your last name'}
-                                optional={false}/>
-
-                        </div>
+                    <div className={'flex'}>
 
                         <Input
                             type={'text'}
-                            name={'username'}
-                            label={'Username'}
-                            placeholder={'Enter your username'}
+                            name={'fname'}
+                            label={'First Name'}
+                            placeholder={'Enter your first name'}
                             optional={false}/>
 
                         <Input
-                            type={'email'}
-                            name={'email'}
-                            label={'Email'}
-                            placeholder={'Enter your email'}
+                            type={'text'}
+                            name={'lname'}
+                            label={'Last Name'}
+                            placeholder={'Enter your last name'}
                             optional={false}/>
 
-                        <Input
-                            type={'password'}
-                            name={'password'}
-                            label={'Password'}
-                            placeholder={'Enter your password'}
-                            optional={false}/>
                     </div>
 
-                    <div className={'text-center mt-5'}>
-                        <button className={'main-btn'}>Sign Up</button>
-                    </div>
+                    <Input
+                        type={'text'}
+                        name={'username'}
+                        label={'Username'}
+                        placeholder={'Enter your username'}
+                        optional={false}/>
 
-                    <div className={'text-center mt-5'}>
-                        Do have an account? <Link to={'/signin'}><span className={'text-blue-600 underline'}>Sign in now</span></Link>
-                    </div>
+                    <Input
+                        type={'email'}
+                        name={'email'}
+                        label={'Email'}
+                        placeholder={'Enter your email'}
+                        optional={false}/>
 
+                    <Input
+                        type={'password'}
+                        name={'password'}
+                        label={'Password'}
+                        placeholder={'Enter your password'}
+                        optional={false}/>
                 </div>
-            </section>
-        );
-    }
+
+                <div className={'text-center mt-5'}>
+                    <button className={'main-btn'}>Sign Up</button>
+                </div>
+
+                <div className={'text-center mt-5'}>
+                    Do have an account? <Link to={'/signin'}><span className={'text-blue-600 underline'}>Sign in now</span></Link>
+                </div>
+
+            </div>
+        </section>
+    );
 }
 
 export default Signup;
