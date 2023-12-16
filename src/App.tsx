73474d872921490e799d46044663f6ef2a09a5a8
tharindu-dasun <tharindu.dasunccp@gmail.com>
2023-12-16 11:45:54 +0530

    update all life-cycle methods

--- src/App.tsx
+++ src/App.tsx
@@ -4,47 +4,17 @@ import Header from "./component/layout/header";
 import Footer from "./component/layout/footer";
 import Home from "./views/home";
 
-interface Props{
-
+interface Props {
 }
 
-interface State{
-    count: number;
+interface State {
+    count: number
 }
 
 class App extends React.Component<Props, State> {
 
-    // state = {
-    //     count: 0
-    // }
-
-    constructor(props: Props) {
-        super(props);
-        this.state = {count: 0}
-        console.log('Constructor  1');
-    }
-
-    static getDerivedStateFromProps(Props , State){
-        console.log('getDerivedStateFromProps  2');
-        return null;
-    }
-
-    componentDidMount(): void {
-        console.log('componentDidMount  3');
-    }
-
-    shouldComponentUpdate(nextProps: Readonly<Props>, nextState: Readonly<State>, nextContext: any): boolean {
-        console.log('Should componentDidUpdate  4');
-        return true;
-    }
-
-    getSnapshotBeforeUpdate(prevProps: Readonly<Props>, prevState: Readonly<State>): any {
-        console.log('Snap Shot  5');
-        return null;
-    }
-
-    componentDidUpdate(prevProps: Readonly<Props>, prevState: Readonly<State>, snapshot?: any) {
-        console.log('Component Did Update  6');
+    state = {
+        count: 0
     }
 
     up = () => {
@@ -55,7 +25,7 @@ class App extends React.Component<Props, State> {
         return (
             <div>
 
-                <Header/>
+                {/*<Header/>*/}
 
                 {/*<Home/>*/}
 
@@ -65,7 +35,12 @@ class App extends React.Component<Props, State> {
                     <button className={'bg-green-600 text-white p-5'} onClick={this.up}>Click</button>
                 </div>
 
-                <Footer/>
+                {
+                    this.state.count > 0 ? null : <Card title={"hi"} content={"hhhhhhhhhhhhhhhhhhhh"}/>
+                }
+
+
+                {/*<Footer/>*/}
 
             </div>
         );
