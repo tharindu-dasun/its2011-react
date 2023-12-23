5dddf5f137be67a13a82b6ce44a1cc8d20213ead
tharindu-dasun <tharindu.dasunccp@gmail.com>
2023-12-23 09:23:26 +0530

    axios

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,57 +1,62 @@
 import React from 'react';
 import Card from "./../component/card/card";
+import {Simulate} from "react-dom/test-utils";
+import error = Simulate.error;
+import axios from "axios";
 
 interface Data {
     id: number,
     title: string,
-    content: string
+    body: string
 }
 
-const data: Data[] = [
-    {
-        id: 1,
-        title: "IJSE",
-        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A impedit ipsa quae tenetur totam. Ad architecto asperiores culpa cupiditate doloremque, dolores minima numquam perferendis quaerat, quasi sequi similique tempore voluptas."
-    },
-    {
-        id: 2,
-        title: "GDSE",
-        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A impedit ipsa quae tenetur totam. Ad architecto asperiores culpa cupiditate doloremque, dolores minima numquam perferendis quaerat, quasi sequi similique tempore voluptas."
-    },
-    {
-        id: 3,
-        title: "Javascript",
-        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A impedit ipsa quae tenetur totam. Ad architecto asperiores culpa cupiditate doloremque, dolores minima numquam perferendis quaerat, quasi sequi similique tempore voluptas."
-    },
-    {
-        id: 4,
-        title: "Typescript",
-        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A impedit ipsa quae tenetur totam. Ad architecto asperiores culpa cupiditate doloremque, dolores minima numquam perferendis quaerat, quasi sequi similique tempore voluptas."
-    },
-    {
-        id: 5,
-        title: "Java",
-        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A impedit ipsa quae tenetur totam. Ad architecto asperiores culpa cupiditate doloremque, dolores minima numquam perferendis quaerat, quasi sequi similique tempore voluptas."
-    },
-    {
-        id: 6,
-        title: "Go",
-        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A impedit ipsa quae tenetur totam. Ad architecto asperiores culpa cupiditate doloremque, dolores minima numquam perferendis quaerat, quasi sequi similique tempore voluptas."
-    },
-];
 
 
-class Home extends React.Component<any, any>{
+class Home extends React.Component<any, any> {
+
+    state = {
+        data: []
+    }
+
+    componentDidMount(): void {
+        this.fetchData();
+    }
+
+    /**
+     * fetch article data
+     */
+    fetchData = (): void => {
+        // fetch('https://jsonplaceholder.typicode.com/posts')
+        //     .then(response => response.json())
+        //     .then(result => {
+        //         console.log(result);
+        //         this.setState({ data: result });
+        //     })
+        //     .catch(error => {
+        //         console.log(error);
+        //     });
+
+        axios.get('https://jsonplaceholder.typicode.com/posts').then(result =>{
+            console.log(result.data);
+        }).catch(err =>{
+            console.log(err);
+        })
+    }
+
+
+
+
+
 
     render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | Iterable<React.ReactNode> | React.ReactPortal | boolean | any | null | undefined {
-        return(
+        return (
             <section>
                 <div
                     className={'grid 2xl:grid-cols-5 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 w-fit relative m-auto'}>
 
                     {
-                        data.map((r: Data, index: number) => {
-                            return <Card title={r.title} content={r.content}/>
+                        this.state.data.map((r: Data, index: number) => {
+                            return <Card title={r.title} content={r.body}/>
                         })
                     }
 
